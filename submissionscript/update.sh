9391738806fce4eba6c4c7701307d1ab6d7984b8
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-02 17:45:25 +0100

    feat: extract var values form script arguments

--- submissionscript/update.sh
+++ submissionscript/update.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-# VARIABLES
-DBHOST=localhost
-DBUSER=daniel
-DBPASS=test_pass
-DBNAME=colibri_test
 
-# REMOVE ALL SPACES FROM FILES NAMES
 # REMOVE ALL SPACES FROM FILES NAMES
 find ../dbscripts/ -depth -name '* *.sql' \
 | while IFS= read -r f ; do mv -i "$f" "${f// /_}"; done
+
+
+# VARIABLES
+# Extract variables values form script's arguments
+if [ "$#" -ge 1 ]; then
+  SCRIPTSPATH=$1
+else
+  IFS= read -r SCRIPTSPATH || exit # on EOF
+fi
+
+if [ "$#" -ge 2 ]; then
+  DBUSER=$2
+else
+  IFS= read -r DBUSER || exit # on EOF
+fi
+
+if [ "$#" -ge 3 ]; then
+  DBHOST=$3
+else
+  IFS= read -r DBHOST || exit # on EOF
+fi
+
+if [ "$#" -ge 4 ]; then
+  DBNAME=$4
+else
+  IFS= read -r DBNAME || exit # on EOF
+fi
+
+if [ "$#" -ge 5 ]; then
+  DBPASS=$5
+else
+  IFS= read -r DBPASS || exit # on EOF
+fi
+
+
+
